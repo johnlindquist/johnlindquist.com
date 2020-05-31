07e94f627bf594865f0ac9c516f50e8cc72547f4
Vojta Holik <vojta@egghead.io>
2020-05-31 20:44:54 +0200

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import JohnSurprised from '../images/logo/john-surprised.svg'
+import Link from '../components/link'
 
 export default () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn't exist... the sadness.</p>
+  <div className="w-screen h-screen flex items-center justify-center flex-col">
+    <img src={JohnSurprised} alt="Surprised John" className="w-32" />
+    <h1 className="mt-5 text-2xl font-semibold">404 — Page Not Found</h1>
+    <Link
+      to="/"
+      className="mt-5 inline-flex items-center p-2 border border-gray-200 text-sm leading-4 font-medium rounded text-gray-700 bg-white hover:text-indigo-600 focus:outline-none focus:border-indigo-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150"
+    >
+      Home
+    </Link>
   </div>
 )
