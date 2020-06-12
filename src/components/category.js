2f2afc89108375367448da5f7a3f9d0c324e2cf2
Vojta Holik <vojta@egghead.io>
2020-06-12 08:54:06 +0200

    💅 format svg

--- src/components/category.js
+++ src/components/category.js
@@ -17,17 +17,8 @@ export default function Category({ posts, categories, location, ...props }) {
           to="/posts"
           className="-mt-px inline-flex items-center text-md leading-5 font-medium text-gray-600 hover:text-gray-700  focus:text-gray-700 focus:border-gray-400 transition ease-in-out duration-150"
         >
-          <svg
-            className="mr-3 h-5 w-5 text-gray-500"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-          >
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
-            />
-          </svg>
+          {/* prettier-ignore */}
+          <svg className="mr-3 h-5 w-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" ><path fillRule="evenodd" clipRule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" /></svg>
           All Posts
         </Link>
         <h1 className="text-4xl font-bold text-center capitalize">
